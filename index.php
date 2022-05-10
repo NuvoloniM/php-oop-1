aef921ba5e01f79bccb99555c295fa041a146f13
NuvoloniM <nuvoloni.m@gmail.com>
2022-05-10 16:11:28 +0200

    constructor

--- index.php
+++ index.php
@@ -11,17 +11,31 @@
  <?php
 //  Creo la classe Movie
 class Movie {
-    // dentro alla classe definisco gli Attributi (variabili d'istanza)
+    // dentro alla classe definisco gli Attributi, variabili d'istanza.
     public $title;
     public $year;
     public $lang;
+    public $color;
 
     // definisco un costruttore, una funzione da invocare quando si crea una istanza di classe. 
     // se ci passo dentro delle variabili, nel momento della creazione di istanza devo associarci un valore.
-    function __construct($_title, $_year, $_lang) {
+    public function __construct($_title, $_year, $_lang) {
         $this -> title = $_title;
         $this -> year = $_year;
         $this -> lang = $_lang;
+    }
+
+    // creo un metodo che sarà valido solo per gli elementi che fanno parte della classe 
+    public function setColor($lang){
+        if ($lang == 'Ita'){
+            $this -> color = 'red';
+        }else {
+            $this -> color = 'black';
+        }
+    }
+
+    public function getColor(){
+        $this -> color;
     }
 }
 
@@ -29,5 +43,12 @@ class Movie {
 $strange = new Movie ("Doctor Strange nel Multiverso della Follia", 2022, "Eng");
 $animali = new Movie ("Fantastic Beasts and Where To Find Them 3", 2022, "Eng");
 $sonic = new Movie ("Sonic 2 – Il Film", 2022, "Eng");
-$angeli ) new Movie ("Il sesso degli angeli", 2022, "Ita");
+$angeli = new Movie ("Il sesso degli angeli", 2022, "Ita");
+
+
+//stampo gli attributi dei film 
+foreach ($strange as $key => $value) {
+    echo "<div>" . $key -> $value . "</div>";
+}
+
 ?>
